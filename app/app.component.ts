ccfed2682258d2bfe7e655d656810673c8662830
Jenn Chan <jennjyc@gmail.com>
2016-09-23 13:33:40 -0700

    updated to Angular 2 release

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,23 +1,11 @@
 import { Component } from "@angular/core";
-import { CORE_DIRECTIVES } from "@angular/common";
-
-import { DROPDOWN_DIRECTIVES } from "ng2-bootstrap";
 
 @Component({
   selector: "app-root",
-  directives: [DROPDOWN_DIRECTIVES, CORE_DIRECTIVES],
   styleUrls: ["../css/app.css"],
   templateUrl: "../html/app.component.html"
 })
 
 export class AppComponent { 
-    status:{isopen:boolean} = {isopen: false};tructor() {}
-
     constructor() {}
-
-    toggleDropdown($event:MouseEvent):void {
-      $event.preventDefault();
-      $event.stopPropagation();
-      this.status.isopen = !this.status.isopen;
-  }
 }
